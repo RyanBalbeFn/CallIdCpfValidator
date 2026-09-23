59ea7c3ac39656886cc77af702b051f131af3646
RyanBalbe.Fn <ryanbalbe.tj@gmail.com>
2026-09-22 22:44:50 -0300

    Add project files.

---
+++ CallRecord.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallIdCpfValidator
+{
+    internal class CallRecord
+    {
+        public required string CallId { get; set; }
+        public required string Cpf {  get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using CallIdCpfValidator;
+
+Console.WriteLine("=== CallID CPF Validator ===");
+Console.WriteLine("Sistema iniciado!");
+
+Console.WriteLine("Digite o CallID");
+
+// Aqui o usuário insere o CallID desejado,
+string callId = Console.ReadLine() ?? "";
+
+// Cria um registro associando o CallID informado ao CPF temporário.
+CallRecord registro = new CallRecord
+{
+    CallId = callId,
+    Cpf = "12345678909"
+};
+
+// Por fim, exibe os dados da chamada encontrados no registro.
+Console.WriteLine("Dados encontrados:");
+Console.WriteLine($"CallID: {registro.CallId}");
+Console.WriteLine($"CPF: {registro.Cpf}");
